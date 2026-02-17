f4a1a44a6fda2482384d3a37521e2f54857761fd
ferzjw <ferz.jw@gmail.com>
2026-02-17 10:44:53 -0300

    add: repaso

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Home } from './components/Home'
+import { test } from './components/test'
 
 import "./styles/main.css"
 
